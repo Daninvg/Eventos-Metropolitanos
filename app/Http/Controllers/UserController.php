d8fc3b7caa19c9b4892a5d72a250c9fb8ba62ff0
Daninvg <cristian.navarrete6778@alumnos.udg.mx>
2026-04-10 10:04:53 -0600

    feature: Se crea el controlador de usuario y se implementa template
    Se crea el controlador de usuario con sus respectivos metodos CRUD. Se implementa plantilla layout para reutilizacion, ademas, se crea la primera vista del para crear usuario

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+       /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('user.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max: 20',
+            'email' => 'required|email |max: 256',
+            'password' => 'required|max: 15| string'
+        ]);
+        $user = User::create($request->all());
+        
+        return redirect()->route('user.index', compact('user'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return view('user.show',compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('user.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required|max: 20',
+            'email' => 'required|email |max: 256',
+            'password' => 'required|max: 15| string'
+        ]);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Laravel\Fortify\Features;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::resource('user', UserController::class);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
